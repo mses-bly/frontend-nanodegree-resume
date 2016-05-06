282bfcf561dd9a1c0b73e6001dfccbc765454fc7
Moises Baly <moises.baly@gmail.com>
2016-05-06 12:21:01 -0400

    Section 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = HTMLheaderName.replace("%data%", "Moises Baly");
+var role = HTMLheaderRole.replace("%data%", "Sofware Engineer");
+
+$("#header").append(name).append(role)
